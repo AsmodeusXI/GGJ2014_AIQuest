25111e7b67207c3ed06f8c1a9c45cd16b07a5871
Denver506 <MaxwellOldt@live.com>
2014-01-24 23:14:15 -0600

    Basics-First Commit
    Yep

---
+++ AIGame/Assets/SpawnMonsterOne.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnMonsterOne : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown()
+	{
+		// ... instantiate the rocket facing right and set it's velocity to the right. 
+		Rigidbody2D bulletInstance = Instantiate(rocket, transform.position, Quaternion.Euler(new Vector3(0,0,0))) as Rigidbody2D;
+		bulletInstance.velocity = new Vector2(speed, 0);
+	}
+}
