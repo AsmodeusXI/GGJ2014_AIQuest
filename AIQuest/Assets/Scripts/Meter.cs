30e3296baa3d04e2c78ea4ca58e5396a1f9ed952
emotl <elliotmotl@gmail.com>
2014-01-25 15:36:22 -0600

    added meter class

---
+++ AIQuest/Assets/Scripts/Meter.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Meter : MonoBehaviour {
+	
+	public float minPos; // -1.6019
+	public float maxPos; // 2.37459
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Min Pos: " + minPos);
+		Debug.Log ("Max Pos: " + maxPos);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	// Takes a value 0-100.  This is translated into the mood bar.
+	// 0 being the bottom and 10 being the top.
+	void UpdateMeter(float mood){
+		float ratio = mood / 100;
+		
+		Vector3 newPos = new Vector3 (transform.position.x, minPos + ((maxPos - minPos) * ratio), transform.position.z);
+		Debug.Log ("delta: " + (maxPos - minPos).ToString());
+		Debug.Log (newPos.ToString ());
+		transform.position = newPos;
+		
+	}
+}
