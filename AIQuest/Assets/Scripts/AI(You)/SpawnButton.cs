c48c21bd6ec144b4b1e4e3e239931b6e050252cd
Westin Breger <westin007@gmail.com>
2014-01-25 12:43:12 -0600

    Cleanup, code consolidation with deploy buttons WKB

---
+++ AIQuest/Assets/Scripts/AI(You)/SpawnButton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SpawnButton : MonoBehaviour {
+	
+	public Transform monster;
+    public string keyboardInput;
+
+
+    KeyCode keyboardButton;
+
+    private Transform buttonPostion;
+	void Start () {
+
+        buttonPostion = GetComponent<Transform>();
+        keyboardButton = (KeyCode)Enum.Parse(typeof(KeyCode), keyboardInput.ToUpper());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(keyboardButton) ){
+			Spawn();
+		}
+	}
+	
+	void OnMouseDown(){
+		Spawn();
+	}
+	
+	void Spawn(){
+		// ... instantiate the rocket facing right and set it's velocity to the right. 
+		Instantiate(monster, buttonPostion.position, Quaternion.identity);
+	}
+}
