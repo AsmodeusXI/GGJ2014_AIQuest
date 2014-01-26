e323a85c83ceb2967ddc3926c060096827c4613f
AsmodeusXI <AsmodeusXI@gmail.com>
2014-01-25 23:25:25 -0600

    Basic flashing buttons for charge.

--- AIQuest/Assets/Scripts/AI(You)/SpawnButton.cs
+++ AIQuest/Assets/Scripts/AI(You)/SpawnButton.cs
@@ -4,10 +4,14 @@ using System;
 
 public class SpawnButton : MonoBehaviour {
 
-	public float chargeLevel;
-	public bool timerOn;
+	private float chargeLevel;
+	private bool timerOn;
 	public Transform monster;
     public string keyboardInput;
+    public GameObject button;
+    private SpriteRenderer spriter;
+    private float chargeMarker = 0.5f;
+    private bool colorSwitch;
     
     KeyCode keyboardButton;
     
@@ -15,32 +19,50 @@ public class SpawnButton : MonoBehaviour {
 	void Start () {
         buttonPosition = GetComponent<Transform>();    
         keyboardButton = (KeyCode)Enum.Parse(typeof(KeyCode), keyboardInput.ToUpper());
-        }
+        spriter = (SpriteRenderer) button.GetComponent<SpriteRenderer>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKeyDown(keyboardButton) ){
 			timerOn = true;
-            }
+        }
+        
 		if (timerOn) {
 			chargeLevel += Time.deltaTime;
+			
+			if (chargeLevel >= chargeMarker) {
+				colorSwitch = !colorSwitch;
+				chargeMarker += chargeMarker;
+			}
+			
+			if(colorSwitch) {
+				spriter.color = Color.yellow;
+			} else {
+				spriter.color = Color.white;
+			}
 		}
+		
 		if (Input.GetKeyUp (keyboardButton)) {
 			Spawn (chargeLevel);
 			chargeLevel = 0;
+			chargeMarker = 0;
 			timerOn = false;
+			spriter.color = Color.white;
 		}
 	}
 	
 	void OnMouseDown(){
 		timerOn = true;
+		spriter.color = Color.red;
 	}
 
 	void OnMouseUp(){
 		Spawn (chargeLevel);
 		chargeLevel = 0;
 		timerOn = false;
-		}
+		spriter.color = Color.white;
+	}
 	
 	void Spawn(float charge){
 		Transform temp = (Transform)Instantiate(monster, buttonPosition.position, Quaternion.identity);
