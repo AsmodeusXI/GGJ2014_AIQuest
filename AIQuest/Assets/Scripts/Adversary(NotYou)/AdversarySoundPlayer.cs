216d6b944daf93acb46d7f8a6c319a8dffa09d54
Westin Breger <westin007@gmail.com>
2014-01-26 15:41:42 -0600

    Adversary makes sounds now

---
+++ AIQuest/Assets/Scripts/Adversary(NotYou)/AdversarySoundPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AdversarySoundPlayer : MonoBehaviour
+{
+    public List<AudioClip> adversarySounds;
+
+    private AudioSource soundPlayer;
+
+    public void Start()
+    {
+        soundPlayer = this.gameObject.GetComponent<AudioSource>();
+    }
+
+    public void play(int index)
+    {
+        if(index >= 0 && index < adversarySounds.Count)
+            soundPlayer.clip = adversarySounds[index];
+        if(!soundPlayer.isPlaying)
+            soundPlayer.Play();
+    }
+}
